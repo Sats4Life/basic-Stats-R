f53a45bcc19d5a57fa5f1c693b1475fb135b164a
FrancoiseCB <90904539+BlancoFC@users.noreply.github.com>
2023-06-18 18:18:19 +0100

    Add T0 files

---
+++ T0_Script.R
@@ -0,0 +1,17 @@
+###########################################
+# Title: "Practical 0"
+# Author: "Your Name"
+# Date: "DD/MM/2022"
+# Description: Script of basic commands used in R
+
+###########################################
+
+# All things that are not part of a command, should be on lines that
+# start with at least  one hash "#" this is how the interpreter in R Studio
+# knows to by-pass it.
+
+# A good code is one that can be understood by anyone who needs
+# to use it, including your future self. Use the comments to
+# explain each instruction to your future self and to strangers.
+
+# Use this R Script file to document what you do during the practical
